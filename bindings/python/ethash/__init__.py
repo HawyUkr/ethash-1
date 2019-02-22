acffd44aa096cd8720b651d48da16ffb0efbf955
Paweł Bylica <chfast@gmail.com>
2019-02-22 16:32:55 +0100

    fixup! Rename keccak256 -> keccak_256 to match pysha3 naming convention

--- bindings/python/ethash/__init__.py
+++ bindings/python/ethash/__init__.py
@@ -1,10 +1,10 @@
 from _ethash import ffi, lib
 
 
-def keccak256(data):
+def keccak_256(data):
     hash = lib.ethash_keccak256(ffi.from_buffer(data), len(data))
     return ffi.string(hash.bytes)
 
-def keccak512(data):
+def keccak_512(data):
     hash = lib.ethash_keccak512(ffi.from_buffer(data), len(data))
     return ffi.string(hash.bytes)
